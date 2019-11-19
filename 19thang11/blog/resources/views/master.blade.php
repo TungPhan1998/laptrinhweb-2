b61c33601b54f782aea18cc12d9b46f7f1abb516
Student <Student@tdc.edu.vn>
2019-11-19 14:30:48 +0700

    19thang11

---
+++ 19thang11/blog/resources/views/master.blade.php
@@ -0,0 +1,132 @@
+<div class="header">
+
+		<div class="container">
+			
+			<div class="logo">
+				<h1 ><a href="index"><b>T<br>H<br>E</b>Big Store<span>The Best Supermarket</span></a></h1>
+			</div>
+			<div class="head-t">
+				<ul class="card">
+					<li><a href="wishlist" ><i class="fa fa-heart" aria-hidden="true"></i>Wishlist</a></li>
+					<li><a href="login" ><i class="fa fa-user" aria-hidden="true"></i>Login</a></li>
+					<li><a href="register" ><i class="fa fa-arrow-right" aria-hidden="true"></i>Register</a></li>
+					<li><a href="about" ><i class="fa fa-file-text-o" aria-hidden="true"></i>Order History</a></li>
+					<li><a href="shipping" ><i class="fa fa-ship" aria-hidden="true"></i>Shipping</a></li>
+				</ul>	
+			</div>
+			
+			<div class="header-ri">
+				<ul class="social-top">
+					<li><a href="#" class="icon facebook"><i class="fa fa-facebook" aria-hidden="true"></i><span></span></a></li>
+					<li><a href="#" class="icon twitter"><i class="fa fa-twitter" aria-hidden="true"></i><span></span></a></li>
+					<li><a href="#" class="icon pinterest"><i class="fa fa-pinterest-p" aria-hidden="true"></i><span></span></a></li>
+					<li><a href="#" class="icon dribbble"><i class="fa fa-dribbble" aria-hidden="true"></i><span></span></a></li>
+				</ul>	
+			</div>
+		
+
+				<div class="nav-top">
+					<nav class="navbar navbar-default">
+					
+					<div class="navbar-header nav_2">
+						<button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						
+					</div> 
+					<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
+						<ul class="nav navbar-nav ">
+
+							<li>
+								<a href="{{url('/')}}" class="hyper "><span>Home</span></a>
+							</li>	
+							@foreach($type as $value)				
+								<li>
+									<a href="{{ url('product/'.$value->type_ID)}}" class="hyper"><span> {{ $value->type_name}} </span></a>
+								</li>
+								
+							@endforeach
+						</ul>
+					</div>
+					</nav>
+					 <div class="cart" >
+						<span class="fa fa-shopping-cart my-cart-icon"><span class="badge badge-notify my-cart-badge"></span></span>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+					
+				</div>			
+</div>
+@yield('noidung')
+
+<div class="footer">
+	<div class="container">
+		<div class="col-md-3 footer-grid">
+			<h3>About Us</h3>
+			<p>Nam libero tempore, cum soluta nobis est eligendi 
+				optio cumque nihil impedit quo minus id quod maxime 
+				placeat facere possimus.</p>
+		</div>
+		<div class="col-md-3 footer-grid ">
+			<h3>Menu</h3>
+			<ul>
+				<li><a href="index">Home</a></li>
+				<li><a href="kitchen">Kitchen</a></li>
+				<li><a href="care">Personal Care</a></li>
+				<li><a href="hold">Household</a></li>						 
+				<li><a href="codes">Short Codes</a></li> 
+				<li><a href="contact">Contact</a></li>
+			</ul>
+		</div>
+		<div class="col-md-3 footer-grid ">
+			<h3>Customer Services</h3>
+			<ul>
+				<li><a href="shipping">Shipping</a></li>
+				<li><a href="terms">Terms & Conditions</a></li>
+				<li><a href="faqs">Faqs</a></li>
+				<li><a href="contact">Contact</a></li>
+				<li><a href="offer">Online Shopping</a></li>						 
+				 
+			</ul>
+		</div>
+		<div class="col-md-3 footer-grid">
+			<h3>My Account</h3>
+			<ul>
+				<li><a href="login">Login</a></li>
+				<li><a href="register">Register</a></li>
+				<li><a href="wishlist">Wishlist</a></li>
+				
+			</ul>
+		</div>
+		<div class="clearfix"></div>
+			<div class="footer-bottom">
+				<h2 ><a href="index"><b>T<br>H<br>E</b>Big Store<span>The Best Supermarket</span></a></h2>
+				<p class="fo-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
+				<ul class="social-fo">
+					<li><a href="#" class=" face"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+					<li><a href="#" class=" twi"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+					<li><a href="#" class=" pin"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+					<li><a href="#" class=" dri"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+				</ul>
+				<div class=" address">
+					<div class="col-md-4 fo-grid1">
+							<p><i class="fa fa-home" aria-hidden="true"></i>12K Street , 45 Building Road Canada.</p>
+					</div>
+					<div class="col-md-4 fo-grid1">
+							<p><i class="fa fa-phone" aria-hidden="true"></i>+1234 758 839 , +1273 748 730</p>	
+					</div>
+					<div class="col-md-4 fo-grid1">
+						<p><a href="mailto:info@example.com"><i class="fa fa-envelope-o" aria-hidden="true"></i>info@example1.com</a></p>
+					</div>
+					<div class="clearfix"></div>
+					
+					</div>
+			</div>
+		<div class="copy-right">
+			<p> &copy; 2016 Big store. All Rights Reserved | Design by  <a href="http://w3layouts.com/"> W3layouts</a></p>
+		</div>
+	</div>
+</div>
---
+++ 19thang11/blog/resources/views/product.blade.php
@@ -0,0 +1,298 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Big store a Ecommerce Online Shopping Category Flat Bootstrap Responsive Website Template| Home :: w3layouts</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta property="og:title" content="Vide" />
+<meta name="keywords" content="Big store Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="{{asset('css/bootstrap.css')}}" rel='stylesheet' type=text/css />
+<!-- Custom Theme files -->
+<link href="{{asset('css/style.css')}}" rel='stylesheet' type='text/css' />
+<!-- js -->
+   <script src="{{asset('js/jquery-1.11.1.min.js')}}"></script>
+<!-- //js -->
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="{{asset('js/move-top.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/easing.js')}}"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+<link href="{{asset('css/font-awesome.css')}}" rel="stylesheet"> 
+<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Noto+Sans:400,700' rel='stylesheet' type='text/css'>
+<!--- start-rate---->
+<script src="js/jstarbox.js"></script>
+	<link rel="stylesheet" href="{{asset('css/jstarbox.css')}}" type="text/css" media="screen" charset="utf-8" />
+		<script type="text/javascript">
+			jQuery(function() {
+			jQuery('.starbox').each(function() {
+				var starbox = jQuery(this);
+					starbox.starbox({
+					average: starbox.attr('data-start-value'),
+					changeable: starbox.hasClass('unchangeable') ? false : starbox.hasClass('clickonce') ? 'once' : true,
+					ghosting: starbox.hasClass('ghosting'),
+					autoUpdateAverage: starbox.hasClass('autoupdate'),
+					buttons: starbox.hasClass('smooth') ? false : starbox.attr('data-button-count') || 5,
+					stars: starbox.attr('data-star-count') || 5
+					}).bind('starbox-value-changed', function(event, value) {
+					if(starbox.hasClass('random')) {
+					var val = Math.random();
+					starbox.next().text(' '+val);
+					return val;
+					} 
+				})
+			});
+		});
+		</script>
+<!---//End-rate---->
+
+</head>
+<body>
+<a href="offer.html"><img src="{{asset('images/download.png')}}" class="img-head" alt=""></a>
+@extends('master')
+@section('noidung')
+  <!---->
+<div data-vide-bg="video/video">
+    <div class="container">
+		<div class="banner-info">
+			<h3>It is a long established fact that a reader will be distracted by 
+			the readable </h3>	
+			<div class="search-form">
+				<form action="#" method="post">
+					<input type="text" placeholder="Search..." name="Search...">
+					<input type="submit" value=" " >
+				</form>
+			</div>		
+		</div>	
+    </div>
+</div>
+
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+    <script src="{{url('js/jquery.vide.min.js')}}"></script>
+
+<!--content-->
+<div class="content-top ">
+	<div class="container ">
+		<div class="spec ">
+			<h3>Special Offers</h3>
+			<div class="ser-t">
+				<b></b>
+				<span><i></i></span>
+				<b class="line"></b>
+			</div>
+
+		</div>
+			<div class="tab-head ">
+				<nav class="nav-sidebar">
+					<ul class="nav tabs ">
+					  <li class="active"><a href="#tab1" data-toggle="tab">Staples</a></li>
+					  <li class=""><a href="#tab2" data-toggle="tab">Snacks</a></li> 
+					  <li class=""><a href="#tab3" data-toggle="tab">Fruits & Vegetables</a></li>  
+					  <li class=""><a href="#tab4" data-toggle="tab">Breakfast & Cereal</a></li>
+					</ul>
+				</nav>
+				<div class=" tab-content tab-content-t ">
+					<div class="tab-pane active text-style" id="tab1">
+						@foreach($data as $value)
+						<div class=" con-w3l">
+							<div class="col-md-3 m-wthree">
+								<div class="col-m">		
+
+									<a href="#" data-toggle="modal" data-target="#myModal1" class="offer-img">
+										<img src="{{asset('images/'. $value->image)}}" class="img-responsive" alt="">
+										<div class="offer"><p><span>Offer</span></p></div>
+									</a>
+									<div class="mid-1">
+										<div class="women">
+											<h6><a href="single.html">{{$value['name']}}</a>(1 kg)</h6>							
+										</div>
+										<div class="mid-2">
+											<p ><label>$2.00</label><em class="item_price">{{$value['price']}}</em></p>
+											  <div class="block">
+												<div class="starbox small ghosting"> </div>
+											</div>
+											<div class="clearfix"></div>
+										</div>
+										<div class="add">
+										   <button class="btn btn-danger my-cart-btn my-cart-b " data-id="1" data-name="Moong" data-summary="summary 1" data-price="1.50" data-quantity="1" data-image="images/of.png">Add to Cart</button>
+										</div>
+										
+									</div>
+								</div>
+							</div>
+							@endforeach
+							<div class="clearfix"></div>
+						 </div>
+					 </div>
+				</div>
+			</div>
+		
+	</div>
+	</div>
+	</div>
+
+<!--content-->
+<div class="content-mid">
+	<div class="container">
+		
+		<div class="col-md-4 m-w3ls">
+			<div class="col-md1 ">
+				<a href="kitchen.html">
+					<img src="images/co1.jpg" class="img-responsive img" alt="">
+					<div class="big-sa">
+						<h6>New Collections</h6>
+						<h3>Season<span>ing </span></h3>
+						<p>There are many variations of passages of Lorem Ipsum available, but the majority </p>
+					</div>
+				</a>
+			</div>
+		</div>
+		<div class="col-md-4 m-w3ls1">
+			<div class="col-md ">
+				<a href="hold.html">
+					<img src="images/co.jpg" class="img-responsive img" alt="">
+					<div class="big-sale">
+						<div class="big-sale1">
+							<h3>Big <span>Sale</span></h3>
+							<p>It is a long established fact that a reader </p>
+						</div>
+					</div>
+				</a>
+			</div>
+		</div>
+		<div class="col-md-4 m-w3ls">
+			<div class="col-md2 ">
+				<a href="kitchen.html">
+					<img src="images/co2.jpg" class="img-responsive img1" alt="">
+					<div class="big-sale2">
+						<h3>Cooking <span>Oil</span></h3>
+						<p>It is a long established fact that a reader </p>		
+					</div>
+				</a>
+			</div>
+			<div class="col-md3 ">
+				<a href="hold.html">
+					<img src="images/co3.jpg" class="img-responsive img1" alt="">
+					<div class="big-sale3">
+						<h3>Vegeta<span>bles</span></h3>
+						<p>It is a long established fact that a reader </p>
+					</div>
+				</a>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<!--content-->
+  <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+         <a href="kitchen.html"> <img class="first-slide" src="images/ba.jpg" alt="First slide"></a>
+       
+        </div>
+        <div class="item">
+         <a href="care.html"> <img class="second-slide " src="images/ba1.jpg" alt="Second slide"></a>
+         
+        </div>
+        <div class="item">
+          <a href="hold.html"><img class="third-slide " src="images/ba2.jpg" alt="Third slide"></a>
+          
+        </div>
+      </div>
+    
+    </div><!-- /.carousel -->
+
+
+<!--footer-->
+@endsection
+<!-- //footer-->
+
+<!-- smooth scrolling -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+		/*
+			var defaults = {
+			containerID: 'toTop', // fading element id
+			containerHoverID: 'toTopHover', // fading element hover id
+			scrollSpeed: 1200,
+			easingType: 'linear' 
+			};
+		*/								
+		$().UItoTop({ easingType: 'easeOutQuart' });
+		});
+	</script>
+	<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+<!-- //smooth scrolling -->
+<!-- for bootstrap working -->
+		<script src="js/bootstrap.js"></script>
+<!-- //for bootstrap working -->
+<script type='text/javascript' src="js/jquery.mycart.js"></script>
+  <script type="text/javascript">
+  $(function () {
+
+    var goToCartIcon = function($addTocartBtn){
+      var $cartIcon = $(".my-cart-icon");
+      var $image = $('<img width="30px" height="30px" src="' + $addTocartBtn.data("image") + '"/>').css({"position": "fixed", "z-index": "999"});
+      $addTocartBtn.prepend($image);
+      var position = $cartIcon.position();
+      $image.animate({
+        top: position.top,
+        left: position.left
+      }, 500 , "linear", function() {
+        $image.remove();
+      });
+    }
+
+    $('.my-cart-btn').myCart({
+      classCartIcon: 'my-cart-icon',
+      classCartBadge: 'my-cart-badge',
+      affixCartIcon: true,
+      checkoutCart: function(products) {
+        $.each(products, function(){
+          console.log(this);
+        });
+      },
+      clickOnAddToCart: function($addTocart){
+        goToCartIcon($addTocart);
+      },
+      getDiscountPrice: function(products) {
+        var total = 0;
+        $.each(products, function(){
+          total += this.quantity * this.price;
+        });
+        return total * 1;
+      }
+    });
+
+  });
+  </script>
+  
+
+</body>
+</html>
